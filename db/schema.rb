af7220b4d0829577c0b2c925f3a18fdab0a712cf
Alexander Nagtegaal <Alexander@Alexanders-MacBook-Pro-2.local>
2016-05-19 17:18:58 +0200

    Version 0.31

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519145624) do
+ActiveRecord::Schema.define(version: 20160519151531) do
 
   create_table "bedrijfstakken", force: :cascade do |t|
     t.string   "naam"
@@ -34,13 +34,6 @@ ActiveRecord::Schema.define(version: 20160519145624) do
     t.integer  "organisatie_id"
     t.string   "boekingtype"
     t.integer  "boekproces_id"
-  end
-
-  create_table "boekproces", force: :cascade do |t|
-    t.string   "naam"
-    t.string   "icoon"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "boekprocessen", force: :cascade do |t|
